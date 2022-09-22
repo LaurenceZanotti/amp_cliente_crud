82536352169ab834db79647541532e6fb207d74e
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-09-22 14:27:06 -0300

    Project first setup

---
+++ README.md
@@ -0,0 +1,36 @@
+# Lista de Clientes (Aplicação CRUD com PHP e PostgreSQL)
+
+Uma aplicação contendo um formulário HTML, uma tabela HTML e scripts PHP para inserção e listagem de dados em/de um SGDB
+
+Feito como exercício prático do módulo 3 da disciplina "Desenv. Web em Html5, Css, Javascript e Php" de Análise e Desenvolvimento de Sistemas da Estácio.
+
+## Sumário
+
+- [Lista de Clientes (Aplicação CRUD com PHP e PostgreSQL)](#lista-de-clientes-aplicação-crud-com-php-e-postgresql)
+  - [Sumário](#sumário)
+  - [Requisitos](#requisitos)
+    - [Preparação](#preparação)
+    - [Motivação](#motivação)
+    - [Esquematizando o banco de dados](#esquematizando-o-banco-de-dados)
+  - [Como rodar](#como-rodar)
+  - [Informação adicional](#informação-adicional)
+
+## Requisitos
+
+Essa seção foi retirada do material online da estácio apenas como uma explicação do projeto.
+
+### Preparação
+
+Este módulo terá caráter prático. Logo, todos os conceitos vistos nos módulos anteriores serão utilizados na confecção de um Formulário HTML, que enviará dados para um script PHP que os inserirá em um SGBD – nesse caso, no PostgreSQL, e de uma página para listagem desses mesmos dados. Cada etapa desse processo será apresentada em detalhes, desde a criação das tabelas no SGBD, passando pela criação do formulário HTML e chegando nos scripts PHP que farão a conexão e inserção dos dados através das classes PDO e PDOStatement.
+
+### Motivação
+
+Deverá ser criado um formulário para armazenar os dados de clientes, contendo o seu nome completo (obrigatório), o seu CPF (obrigatório; apenas os números), um endereço de e-mail válido (obrigatório) e sua data de nascimento (obrigatório; dia, mês e ano). Além disso, deverá ser criada uma listagem para exibição dos clientes cadastrados.
+
+### Esquematizando o banco de dados
+
+Os dados dos clientes deverão ser armazenados em uma tabela. Tal tabela deverá possuir um campo identificador, autoincremental e único.
+
+## Como rodar
+
+## Informação adicional
---
+++ src/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <title>Lista de Clientes CRUD</title>
+</head>
+<body>
+    <h1>Lista de Clientes</h1>
+    <form>
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome do cliente</label>
+            <input type="text" class="form-control" id="nome">
+        </div>
+        <div class="mb-3">
+            <label for="cpf" class="form-label">CPF</label>
+            <input type="text" class="form-control" id="cpf">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <div class="mb-3">
+            <label for="datanascimento" class="form-label">Data de nascimento</label>
+            <input type="text" class="form-control" id="datanascimento">
+        </div>
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </form>
+    <h2>Cadastrados</h2>
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">CPF</th>
+            <th scope="col">Email</th>
+            <th scope="col">Data de Nascimento</th>
+            </tr>
+        </thead>
+        <?php
+        /*
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+        */
+        ?>
+    </table>
+</body>
+</html>
